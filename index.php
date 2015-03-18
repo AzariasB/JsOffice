29383724d27edb92d69839f805a6880a8e071a74
Zzzaza <poubelle.azarias@gmail.com>
2015-03-18 08:30:48 +0100

    Organisation

--- index.php
+++ index.php
@@ -10,8 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+      
+        <script src="assets/js/main.js"></script>
     </body>
 </html>
