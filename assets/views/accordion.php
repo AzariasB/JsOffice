5591f7714f1bc46d57af0e0fb93d534296649dbc
belinp <belinp@pc-dg-033-05.iut2.upmf-grenoble.fr>
2015-04-01 15:42:56 +0200

    LOLOL

--- assets/views/accordion.php
+++ assets/views/accordion.php
@@ -148,20 +148,40 @@
 </div>
 <div >
     <h3>Calendrier</h3>
-    <div>
+    <div id="calendrier">
         <script>
-                  $(function() {
-                      $("#datepicker").datepicker();
-                  });
+                    $(function() {
+                        $("#datepicker").datepicker();
+                    });
         </script>
         <div id="datepicker"></div>
     </div>
 </div>
 <div>
     <h3>Jeu</h3>
-    <div>
-        <p>HAHAHAHAHAHHA</p>
+    <div id="jeu">
+        <table style="margin:0 0 10px 0; width:100%; background:#fff; border:1px solid #F3F3F3;" cellspacing="0" cellpadding="0">
+            <tr>
+                <td style="font-family:verdana; font-size:11px; color:#000; padding:5px 5px;">
+                    <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="640" height="480">
+                        <param name="movie" value="http://www.jeuxclic.com/jeux/54c5730da2418.swf">
+                        <param name="quality" value="high"></param>
+                        <param name="menu" value="false"></param>
+                        <embed src="http://www.jeuxclic.com/jeux/54c5730da2418.swf" width="360" height="280" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" menu="false" wmode="direct"></embed>
+                    </object>
+                </td>
+            </tr>
+        </table>
     </div>
 </div>
+<div >
+    <h3>Musique</h3>
+    <div id="calendrier">
+        <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/43594047&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+    </div>
+</div>
+<div>
+
+
 
 
