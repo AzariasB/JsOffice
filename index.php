a4a5964f28ff382882fb392ac02197dac1a04da6
AzariasB <azarias.boutin@gmail.com>
2015-04-01 15:53:45 +0200

    Ajout du logo de JsOffice

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ Un bureau virtuel réalisé en HTML5/CSS3/Js ecma5
         <!-- Pour le chargement de la page -->
         <script src="assets/js/pace.js"></script>
         <link href="assets/css/pace_style.css" rel="stylesheet" >
-        
+
         <!--Liens css-->
         <!--<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">-->
         <link href="assets/css/mystyle.css" rel="stylesheet">
@@ -27,7 +27,7 @@ Un bureau virtuel réalisé en HTML5/CSS3/Js ecma5
     <body>
 
         <!--Tout les liens javascript, à ajouter dans le bone ordre-->
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
         <script src = "assets/js/main.js" ></script>
         <!-- Tout le javascript de l'éditeur de texte-->
@@ -35,9 +35,9 @@ Un bureau virtuel réalisé en HTML5/CSS3/Js ecma5
         <!-- Toutes les fonctions js qui permettent de faire des accès ajax à la BDD et sauvegarder/récupérer des données-->
         <script src="assets/js/the_saver.js"></script>
 
-        <div id="jsOffice" >
-            <h1>JsOffice</h1>
 
+        <div id="jsOffice" >
+            <img id="logo_jsoffice" src="assets/images/logo.png">
             <div id="kernel">
                 <!-- Partie de gauche : l'accordéon -->
                 <div id="leftSide" >
